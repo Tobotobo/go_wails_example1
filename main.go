745ae5b7531e90fe46feefb6b624a049b654d478
Tobotobo <tobotobo.channel@gmail.com>
2023-08-22 00:09:10 +0900

    シンプルに改造

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "myproject",
-		Width:  1024,
-		Height: 768,
+		Width:  800,
+		Height: 600,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 240, G: 240, B: 240, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
